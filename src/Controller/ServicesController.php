6a5f38f05a32ead8fe6b79653b86222e1b6a9b0c
rmedard <00Derfine@5>
2018-09-23 21:28:05 +0200

    W10

---
+++ src/Controller/ServicesController.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: medar
+ * Date: 23/09/2018
+ * Time: 21:22
+ */
+
+namespace Drupal\jix_interface\Controller;
+
+
+use Drupal\Core\Controller\ControllerBase;
+
+class ServicesController extends ControllerBase {
+
+    public function content(){
+        return[
+            '#theme' => 'pages.jix_services'
+        ];
+    }
+
+}
