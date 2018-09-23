4abfb698a26ee0c958a36e63fa23525283293326
rmedard <00Derfine@5>
2018-09-23 22:45:57 +0200

    W10

--- src/Controller/ManagerPageController.php
+++ src/Controller/ManagerPageController.php
@@ -15,13 +15,12 @@ use Drupal\Core\Controller\ControllerBase;
  * Class ServicesController
  * @package Drupal\jix_interface\Controller
  */
-class ServicesController extends ControllerBase {
+class ManagerPageController extends ControllerBase {
 
     public function content(){
 
         $renderable = [
-            '#theme' => 'jix_our_services',
-            '#test_var' => 'test variable',
+            '#theme' => 'jix_manager'
         ];
         return $renderable;
     }
